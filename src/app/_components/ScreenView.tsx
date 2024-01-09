c93a25310c6f8c7caa5d4d60f521697deeab2574
ericleonen <ericleonen@gmail.com>
2024-01-09 12:31:03 -0800

    update home page ui

---
+++ src/app/_components/ScreenView.tsx
@@ -0,0 +1,17 @@
+type ScreenViewProps = {
+    children: React.ReactNode,
+    centered?: boolean
+}
+
+export default function ScreenView({ children, centered }: ScreenViewProps) {
+    return (
+        <div 
+            style={{
+                justifyContent: centered ? "center" : "flex-start"
+            }}
+            className="flex flex-col items-center w-screen h-screen"
+        >
+            {children}
+        </div>
+    )
+}
