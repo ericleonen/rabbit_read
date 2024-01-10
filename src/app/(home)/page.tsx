837a4df066e417f4d81ee6f18f55518582597d4f
ericleonen <ericleonen@gmail.com>
2024-01-10 13:50:35 -0800

    initialize play page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <h1 className="text-4xl font-bold text-black">Rabbit, Read!</h1>
           <p className="mt-1 text-lg font-medium text-slate-500">The fast, fun, and free way to practice speed reading!</p>
           <Link 
-            href="#"
+            href="/play"
             className="p-2 mt-3 font-medium border-2 rounded-md border-sky-500 text-sky-500 hover:bg-sky-500/25"
           >
             Get started now
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <header className="w-full py">
-
+        <header className="w-full py-2">
+            <h1 className="text-lg mx-auto text-black font-bold">Rabbit, Read!</h1>
         </header>
     )
 }
