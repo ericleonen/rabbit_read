7419c677259f7a28726eafd17dbf622217b487ed
ericleonen <ericleonen@gmail.com>
2024-01-09 21:40:39 -0800

    write story generation API

--- src/app/api/story/helpers.ts
+++ src/app/api/story/helpers.ts
@@ -1,8 +1,40 @@
+import openai from "@/openai";
+
+/**
+ * Randomly selects n items from arr
+ * @param arr 
+ * @param n count of the number of items to select from arr
+ * @returns
+ * @throws an Error if n is less than or equal to 0 or is larger than the number of items in arr
+ */
 export function randomlySelect<T>(arr: T[], n: number) {
     if (n <= 0) {
         throw new Error("n must be greater than 0");
+    } else if (n >= arr.length) {
+        throw new Error("n must be smaller than or equal to the length of arr");
     }
 
     const shuffled = arr.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, n);
+}
+
+/**
+ * Generates a short story using an array of given subjects and length (in words)
+ * @param subjects 
+ * @param numWords 
+ */
+export async function generateStory(subjects: string[], numWords: number) {
+    let subjectsString = subjects.map((subject: string) => `${subject}, `);
+    subjectsString = subjectsString.slice(0, subjectsString.length - 2);
+
+    const storyGenerator = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo-1106",
+        messages: [{
+            role: "user",
+            content: `Write an approximately ${numWords}-word story about the following subjects:
+                ${subjectsString}`
+        }]
+    });
+
+    return storyGenerator.choices[0].message.content;
 }
--- src/app/api/story/route.ts
+++ src/app/api/story/route.ts
@@ -1,10 +1,34 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import SUBJECTS from "./subjects";
-import { randomlySelect } from "./helpers";
+import { generateStory, randomlySelect } from "./helpers";
+
+type ResponseBody = {
+    story: string,
+    ok: boolean,
+    error: string | null
+}
 
 /**
- * Returns a random approximately 250 word story given three subjects
+ * Returns a random approximately 250 word story given three subjects. If the operation fails,
+ * denotes, in the returned JSON, that a field "ok" is false and an error field
  */
 export async function GET(req: NextRequest) {
-    const subjects = randomlySelect(SUBJECTS, 3);
+    try {
+        const subjects = randomlySelect(SUBJECTS, 3);
+        const story = await generateStory(subjects, 250);
+
+        if (!story) throw new Error("Story generation failed");
+
+        return NextResponse.json({
+            story,
+            ok: true,
+            error: null
+        } as ResponseBody);
+    } catch (err) {
+        return NextResponse.json({
+            story: "",
+            ok: false,
+            error: (err as Error).message
+        });
+    }
 }
