57c6a35b51a55a2bef3cd43defbce06e93e00df9
ericleonen <ericleonen@gmail.com>
2024-01-09 16:57:30 -0800

    update home ui

--- src/app/_components/ScreenView.tsx
+++ src/app/_components/ScreenView.tsx
@@ -9,7 +9,7 @@ export default function ScreenView({ children, centered }: ScreenViewProps) {
             style={{
                 justifyContent: centered ? "center" : "flex-start"
             }}
-            className="flex flex-col items-center w-screen h-screen"
+            className="flex flex-col items-center w-screen h-screen bg-white"
         >
             {children}
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
@@ -12,8 +13,14 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      }
     },
+    colors: {
+      black: "#36454F",
+      white: "#F5F5F5",
+      sky: colors.sky,
+      slate: colors.slate
+    }
   },
   plugins: [],
 }
