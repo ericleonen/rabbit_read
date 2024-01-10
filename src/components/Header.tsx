fafbc08e7557235bdfce00989ea41affef371964
ericleonen <ericleonen@gmail.com>
2024-01-10 13:52:33 -0800

    create header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <header className="w-full py-2">
-            <h1 className="text-lg mx-auto text-black font-bold">Rabbit, Read!</h1>
+        <header className="w-full py-2 flex justify-center border-b-2 border-slate-300">
+            <Link href="/">
+                <h1 className="text-lg text-black font-bold">
+                    Rabbit, Read!
+                </h1>
+            </Link>
         </header>
     )
 }
