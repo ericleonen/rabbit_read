8cc4b4b30a8711ab34a27695cbd3faa1449ba85e
ericleonen <ericleonen@gmail.com>
2024-01-10 18:29:41 -0800

    remove showBeginModal export in layout

--- src/app/play/layout.tsx
+++ src/app/play/layout.tsx
@@ -2,9 +2,6 @@ import Header from "@/components/Header";
 import ScreenView from "@/components/ScreenView";
 import { ParentProps } from "@/types/props";
 import Slider from "./components/Slider";
-import { atom } from "jotai";
-
-export const showBeginModal = atom<boolean>(false);
 
 export default function PlayLayout({ children }: ParentProps) {
     return (
