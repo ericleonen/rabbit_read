43fdcd68c6f347031c3a0f0e2ea109107618fc3a
ericleonen <ericleonen@gmail.com>
2024-01-16 21:54:42 -0800

    add icon and ad sense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,21 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Rabbit, Read!',
   description: 'The fast, fun, and free way to practice speed reading',
+  icons: {
+    icon: "/rabbits/tiny.png"
+  }
 }
 
 export default function RootLayout({ children }: ParentProps) {
   return (
     <html lang="en">
+      <head>
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8343995374377986"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={inter.className}>
         <JotaiProvider>
           {children}
